02b613b3636f52dcebef7f66c4099adfc139dd91
valerie lecoeur <valerie.lecoeur75@gmail.com>
2023-12-26 13:41:42 +0100

    amelioration

--- src/app/Components/pokemon-item.component.ts
+++ src/app/Components/pokemon-item.component.ts
@@ -3,7 +3,15 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-pokemon-item',
     templateUrl: './pokemon-item.component.html',
+    styleUrls:['./pokemon-item.component.scss'],
 })
 export class PokemonItemComponent {
- 
+ name = "bulbizarre";
+ level = 10;
+ sex = Math.random() > 0.5 ? 'male':'female';
+
+
+ getName(){
+    return this.name;
+ }
 }
--- src/app/Components/pokemon-list/pokemon-list.component.ts
+++ src/app/Components/pokemon-list/pokemon-list.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './pokemon-list.component.scss'
 })
 export class PokemonListComponent {
-  
+  canClickButton = false;
+  currentPokemonName = 'carapuce';
+  notificationText = '';
+  pokemonAdded = false;
+  pokemons: string[] = ['carapuce','dracofeu','salameche','pikachu'];
+
+  onAddButtonClick(){
+    this.pokemonAdded = true;
+    this.pokemons.push(this.currentPokemonName);
+  }
+
+  onPokemonChange(event:Event){
+    console.log('change');
+    const inputElement = event.target as HTMLInputElement;
+    this.currentPokemonName = inputElement?.value;
+
+  }
+  constructor() {
+    setTimeout(()=>{
+      this.canClickButton = true;
+    }, 2000);
+  }
 }
